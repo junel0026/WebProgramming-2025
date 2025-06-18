70be1c36964cfb4b2e8649535f6ffa9075d2568b
junel0026 <junelsinmoe@gmail.com>
2025-06-18 12:14:55 +0630

    adding images folder

---
+++ groupThree.js
@@ -0,0 +1,25 @@
+// do --while loop
+
+// let i = 1;  //start
+// do {
+//     console.log (i)  //ending
+//     i++;  //increment
+// } while (i<5); 
+
+
+
+// for (--in--)
+
+let a;
+var MyFruits = new Array ( );
+
+MyFruits[0] = "Apple";
+MyFruits[1] = "Orange";
+MyFruits[2] = "Mango";
+
+for (a in MyFruits) {
+   // console.log(a);
+   //console.log(MyFruits[0]);
+  // console.log(a + " " + MyFruits[a]);
+   console.log(a + " " + MyFruits[0]);
+}
